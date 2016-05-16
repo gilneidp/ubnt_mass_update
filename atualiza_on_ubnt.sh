9d11e0240294c6ca1d695ab29c740a4d0d956685
gilneidp <gilnei@gilnei.com.br>
2016-05-16 15:45:14 -0300

    Update atualiza_on_ubnt.sh

--- atualiza_on_ubnt.sh
+++ atualiza_on_ubnt.sh
@@ -8,10 +8,10 @@ if echo "$VAR" | egrep "5.6.5" >/dev/null;then
   exit
 fi
 if echo "$VAR" | egrep 'XM' >/dev/null;then
-  wget http://www.atuahosting.com.br/xm/fwupdate.bin
+  wget http://www.seudominio.com.br/xm/fwupdate.bin
   ubntbox fwupdate.real -m /tmp/fwupdate.bin
 else
-  wget http://www.atuahosting.com.br/xw/fwupdate.bin
+  wget http://www.seudominio.com.br/xw/fwupdate.bin
   ubntbox fwupdate.real -m /tmp/fwupdate.bin
 fi
 exit 1
