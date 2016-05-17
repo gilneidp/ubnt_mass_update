8390a6396eb9b964ca1c7a0ba6b61ac49ff243b8
gilneidp <gilnei@gilnei.com.br>
2016-05-17 18:30:16 -0300

    Script Modificado

--- Para MK_SOLUTION/atualiza.py
+++ Para MK_SOLUTION/atualiza.py
@@ -8,18 +8,16 @@ import psycopg2
 
 try:
     conn=psycopg2.connect("host='IP_D0_Banco' dbname='NomeDoBanco' user='UsuarioBanco' password='SenhaBanco'")
+	cur = con.cursor()    
+    cur.execute("SELECT framedipaddress, senha_equipamento from mk_conexoes")
+	result = cur.fetchall()
+	count = result.__len__()
+	for i in result:
+		print " TENTANDO CONECTAR NO IP: "  +  i[0] + " Senha: "  + i[1]
+		#os.system("sshpass -p " + i[1] + " ssh -o StrictHostKeyChecking=no admin@" + i[0] + " 'rm -f atualiza.sh'")
+		#os.system("sshpass -p " + i[1] + " ssh -o StrictHostKeyChecking=no admin@" + i[0] + " 'wget -qO- https://wwww.SEUDOMINIO.COM/atualiza_on_ubnt.sh | sh'&")	
 except:
-    print "Nao foi possivel conectar a base :("
+    print "Nao foi possivel Executar Operacoes:("
     
-cur = conn.cursor()
-try:
-    cur.execute("""SELECT framedipaddress, senha_equipamento from mk_conexoes""")
-except:
-    print "Nao foi possivel buscar informacoes!"
-	
-result = cur.fetchall()
-count = result.__len__()
-for i in result:
-     print " TENTANDO CONECTAR NO IP: "  +  i[0] + " Senha: "  + i[1]
-     os.system("sshpass -p " + i[1] + " ssh -o StrictHostKeyChecking=no admin@" + i[0] + " 'rm -f atualiza.sh'")
-     os.system("sshpass -p " + i[1] + " ssh -o StrictHostKeyChecking=no admin@" + i[0] + " 'wget -qO- https://wwww.SEUDOMINIO.COM/atualiza_on_ubnt.sh | sh'&")
+
+
